789eb25d7966e22e2cf6f74b239fdded06308195
Hauke Henning Selke <haukeselke@gmail.com>
2023-12-27 16:12:25 +0800

    update hiragana drill testing

---
+++ files/japanese/jlpt5/jlpt5.js
@@ -0,0 +1,316 @@
+
+let text = 
+[
+  {
+    number: 1,
+    hiragana: "ああ",
+    kanji: " ",
+    type: "(感)",
+    meaning: "Oh!, Ah!",
+    romaji: "aa"
+  },
+  {
+    number: 2,
+    hiragana: "あう",
+    kanji: "【会う】",
+    type: "(動I)",
+    meaning: "meet",
+    romaji: "au"
+  },
+  {
+    number: 3,
+    hiragana: "あおい",
+    kanji: "【青い】",
+    type: "(形)",
+    meaning: "blue",
+    romaji: "aoi"
+  },
+  {
+    number: 4,
+    hiragana: "あかい",
+    kanji: "【赤い】",
+    type: "(形)",
+    meaning: "red",
+    romaji: "akai"
+  },
+  {
+    number: 5,
+    hiragana: "あかるい",
+    kanji: "【明るい】",
+    type: "(形)",
+    meaning: "bright",
+    romaji: "akarui"
+  },
+  {
+    number: 6,
+    hiragana: "あき",
+    kanji: "【秋】",
+    type: "(名)",
+    meaning: "autumn, fall",
+    romaji: "aki"
+  },
+  {
+    number: 7,
+    hiragana: "あく",
+    kanji: "【開く】",
+    type: "(動I)",
+    meaning: "open, to be opened",
+    romaji: "aku"
+  },
+  {
+    number: 8,
+    hiragana: "あける",
+    kanji: "【開ける】",
+    type: "(動II)",
+    meaning: "to open",
+    romaji: "akeru"
+  },
+  {
+    number: 9,
+    hiragana: "あげる",
+    kanji: "【上げる】",
+    type: "(動II)",
+    meaning: "raise, give",
+    romaji: "ageru"
+  },
+  {
+    number: 10,
+    hiragana: "あさ",
+    kanji: "【朝】",
+    type: "(名)",
+    meaning: "morning",
+    romaji: "asa"
+  },
+  {
+    number: 11,
+    hiragana: "あさごはん",
+    kanji: "【朝御飯】",
+    type: "(名)",
+    meaning: "breakfast",
+    romaji: "asagohan"
+  },
+  {
+    number: 12,
+    hiragana: "あさって",
+    kanji: "【明後日】",
+    type: "(名)",
+    meaning: "the day after tomorrow",
+    romaji: "asatte"
+  },
+  {
+    number: 13,
+    hiragana: "あし",
+    kanji: "【足】",
+    type: "(名)",
+    meaning: "foot, leg",
+    romaji: "ashi"
+  },
+  {
+    number: 14,
+    hiragana: "あした",
+    kanji: "【明日】",
+    type: "(名)",
+    meaning: "tomorrow",
+    romaji: "ashita"
+  },
+  {
+    number: 15,
+    hiragana: "あそこ",
+    kanji: "【彼処】",
+    type: "(代)",
+    meaning: "there, over there",
+    romaji: "asoko"
+  },
+  {
+    number: 16,
+    hiragana: "あそぶ",
+    kanji: "【遊ぶ】",
+    type: "(動I)",
+    meaning: "to play, to amuse oneself",
+    romaji: "asobu"
+  },
+  {
+    number: 17,
+    hiragana: "あたたかい",
+    kanji: "【暖かい】",
+    type: "(形)",
+    meaning: "warm",
+    romaji: "atatakai"
+  },
+  {
+    number: 18,
+    hiragana: "あたま",
+    kanji: "【頭】",
+    type: "(名)",
+    meaning: "head, brains",
+    romaji: "atama"
+  },
+  {
+    number: 19,
+    hiragana: "あたらしい",
+    kanji: "【新しい】",
+    type: "(形)",
+    meaning: "new, fresh",
+    romaji: "atarashii"
+  },
+  {
+    number: 20,
+    hiragana: "あちら",
+    kanji: "【彼方】",
+    type: "(代)",
+    meaning: "there, that",
+    romaji: "achira"
+  },
+  {
+    number: 21,
+    hiragana: "あつい",
+    kanji: "【暑い】",
+    type: "(形)",
+    meaning: "hot (weather, etc), warm",
+    romaji: "atsui"
+  },
+  {
+    number: 22,
+    hiragana: "あつい",
+    kanji: "【熱い】",
+    type: "(形)",
+    meaning: "hot (things), heated",
+    romaji: "atsui"
+  },
+  {
+    number: 23,
+    hiragana: "あつい",
+    kanji: "【厚い】",
+    type: "(形)",
+    meaning: "thick",
+    romaji: "atsui"
+  },
+  {
+    number: 24,
+    hiragana: "あと",
+    kanji: "【後】",
+    type: "(名)",
+    meaning: "back, rear, after, later, the rest",
+    romaji: "ato"
+  },
+  {
+    number: 25,
+    hiragana: "あなた",
+    kanji: "【貴方】",
+    type: "(代)",
+    meaning: "you, dear (what a wife calls a husband)",
+    romaji: "anata"
+  },
+  {
+    number: 26,
+    hiragana: "あに",
+    kanji: "【兄】",
+    type: "(名)",
+    meaning: "elder brother",
+    romaji: "ani"
+  },
+  {
+    number: 27,
+    hiragana: "あね",
+    kanji: "【姉】",
+    type: "(名)",
+    meaning: "elder sister",
+    romaji: "ane"
+  },
+  {
+    number: 28,
+    hiragana: "あの",
+    kanji: "【彼の】",
+    type: "(連体)",
+    meaning: "that, those",
+    romaji: "ano"
+  },
+  {
+    number: 29,
+    hiragana: "あの",
+    kanji: " ",
+    type: "(感)",
+    meaning: "er, umm, uh",
+    romaji: "ano"
+  },
+  {
+    number: 30,
+    hiragana: "アパート",
+    kanji: " ",
+    type: "(名)",
+    meaning: "apartment, apartment building",
+    romaji: "apāto"
+  },
+  {
+    number: 31,
+    hiragana: "あびる",
+    kanji: "【浴びる】",
+    type: "(動II)",
+    meaning: "to pour on oneself, to bathe in",
+    romaji: "abiru"
+  },
+  {
+    number: 32,
+    hiragana: "あぶない",
+    kanji: "【危ない】",
+    type: "(形)",
+    meaning: "dangerous",
+    romaji: "abunai"
+  },
+  {
+    number: 33,
+    hiragana: "あまい",
+    kanji: "【甘い】",
+    type: "(形)",
+    meaning: "sweet",
+    romaji: "amai"
+  },
+  {
+    number: 34,
+    hiragana: "あまり",
+    kanji: "【余り】",
+    type: "(副)",
+    meaning: "not very (with negative sentence), not much",
+    romaji: "amari"
+  },
+  {
+    number: 35,
+    hiragana: "あめ",
+    kanji: "【雨】",
+    type: "(名)",
+    meaning: "rain",
+    romaji: "ame"
+  },
+  {
+    number: 36,
+    hiragana: "あらう",
+    kanji: "【洗う】",
+    type: "(動I)",
+    meaning: "to wash",
+    romaji: "arau"
+  },
+  {
+    number: 37,
+    hiragana: "ある",
+    kanji: " ",
+    type: "(動I)",
+    meaning: "to be (usu. of inanimate objects), to exist, to be located, to have, to possess, to take place",
+    romaji: "aru"
+  },
+  {
+    number: 38,
+    hiragana: "あるく",
+    kanji: "【歩く】",
+    type: "(動I)",
+    meaning: "to walk",
+    romaji: "aruku"
+  },
+  {
+    number: 39,
+    hiragana: "あれ",
+    kanji: " ",
+    type: "(代)",
+    meaning: "that, that one",
+    romaji: "are"
+  }
+]
