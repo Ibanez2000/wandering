8cb1c8a7fbc3e4c99d68f3fbbedd50104fbc9ca8
Hauke Henning Selke <haukeselke@gmail.com>
2023-12-29 18:07:40 +0800

    update

--- files/japanese/anime/anime.js
+++ files/japanese/anime/anime.js
@@ -3,119 +3,136 @@ let anime = [
     number: 0,
     hiragana: "いってきます",
     kanji: "行ってきます",
-    english: "I'm leaving!",
+    type: "",
+    meaning: "I'm leaving!",
     romaji: "ittekimasu"
   },
   {
     number: 1,
     hiragana: "いくぞ!",
     kanji: "-",
-    english: "Let's go!",
+    type: "",
+    meaning: "Let's go!",
     romaji: "ikuzo"
   },
   {
     number: 2,
     hiragana: "あらあら",
     kanji: "-",
-    english: "Oh my",
+    type: "",
+    meaning: "Oh my",
     romaji: "araara"
   },
   {
     number: 3,
     hiragana: "ごちそうさま",
     kanji: "ご馳走様",
-    english: "thank you (for the meal)",
+    type: "",
+    meaning: "thank you (for the meal)",
     romaji: "gochisousama"
   },
   {
     number: 4,
     hiragana: "あくま",
     kanji: "悪魔",
-    english: "demon",
+    type: "",
+    meaning: "demon",
     romaji: "akuma"
   },
   {
     number: 5,
     hiragana: "ほんと?",
     kanji: "-",
-    english: "Really?",
+    type: "",
+    meaning: "Really?",
     romaji: "honto"
   },
   {
     number: 6,
     hiragana: "ただいま",
     kanji: "只今",
-    english: "I'm home!",
+    type: "",
+    meaning: "I'm home!",
     romaji: "tadaima"
   },
   {
     number: 7,
     hiragana: "へんたい",
     kanji: "変態",
-    english: "Pervert!",
+    type: "",
+    meaning: "Pervert!",
     romaji: "hentai"
   },
   {
     number: 8,
     hiragana: "ちょっとまって",
     kanji: "ちょっと待って",
-    english: "wait a minute",
+    type: "",
+    meaning: "wait a minute",
     romaji: "chotto matte"
   },
   {
     number: 9,
     hiragana: "ごめんなさい",
     kanji: "-",
-    english: "I am sorry.",
+    type: "",
+    meaning: "I am sorry.",
     romaji: "gomen nasai"
   },
   {
     number: 10,
     hiragana: "ばか",
     kanji: "-",
-    english: "idiot",
+    type: "",
+    meaning: "idiot",
     romaji: "baka"
   },
   {
     number: 11,
     hiragana: "ばけもの",
     kanji: "化け物",
-    english: "monster",
+    type: "",
+    meaning: "monster",
     romaji: "bakemono (Also: next level!)"
   },
   {
     number: 12,
     hiragana: "だいじょうぶ",
     kanji: "大丈夫",
-    english: "I'm fine",
+    type: "",
+    meaning: "I'm fine",
     romaji: "daijoubu"
   },
   {
     number: 13,
     hiragana: "いただきます",
     kanji: "-",
-    english: "thank you (for the meal just served)",
+    type: "",
+    meaning: "thank you (for the meal just served)",
     romaji: "itadakimasu"
   },
   {
     number: 14,
     hiragana: "あばよ",
     kanji: "-",
-    english: "bye! (only anime)",
+    type: "",
+    meaning: "bye! (only anime)",
     romaji: "abayo"
   },
   {
     number: 15,
     hiragana: "いたい",
     kanji: "痛い",
-    english: "it hurts",
+    type: "",
+    meaning: "it hurts",
     romaji: "itai"
   },
   {
     number: 16,
     hiragana: "がんばれ",
     kanji: "-",
-    english: "do your best!",
+    type: "",
+    meaning: "do your best!",
     romaji: "ganbare"
   }
 ]
