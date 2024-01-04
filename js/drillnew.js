08175b5388630b09f214ef08810906617002d72c
Hauke Henning Selke <haukeselke@gmail.com>
2024-01-04 11:15:38 +0800

    update

--- js/drillnew.js
+++ js/drillnew.js
@@ -35,30 +35,59 @@ makeRequests();
 // Reminder: we cannot continue with code on the top level because we don't know when makeRequests finishes, so we have to initiate everything else from within there
 
 function main(data) {
+  // let dataNew = data;
+
   dataNew = data;
+
+
+ keys = Object.keys(dataNew);
+
+
+  
+  console.log(dataNew.keys());
+
+  dataNew.forEach(function (item) {
+    // console.log(item.keys());
+
+    designation = "";
+
+    // console.log(item.JAanime);
+  });
+
+  vocab = [];
+
+  // for each element
+
   //console.log(dataNew);
-  createVocabularySelector();
- console.log("hello");  
+  // createVocabularySelector();
+  // console.log("hello");
+
+  createVocabularySelector(dataNew);
+
+  // console.log(vocSelected);
 }
 
+function createVocabularySelector(dataNew) {
+  const options = dataNew.map((obj) => Object.keys(obj)[0]);
 
-function createVocabularySelector(){
+  var vocabularySelectorSelect = document.createElement("select");
+  vocabularySelectorSelect.setAttribute("id", "vocabularySelectorSelect");
+  document
+    .getElementById("fieldContainer")
+    .appendChild(vocabularySelectorSelect);
 
-  // var options = [];
-        var options = ["Option 1", "Option 2", "Option 3"];
+  options.forEach(function (optionText) {
+    var optionElement = document.createElement("option");
+    optionElement.text = optionText;
+    vocabularySelectorSelect.add(optionElement);
+  });
 
+  document.getElementById("vocabularySelectorSelect").onchange = vocSelected;
+  vocSelected();
+}
 
-        
-
-        var vocabularySelectorSelect = document.createElement("select");
-        vocabularySelectorSelect.setAttribute("id", "vocabularySelectorSelect");
-        document.getElementById("fieldContainer").appendChild(vocabularySelectorSelect);
-
-        
-
-        options.forEach(function (optionText) {
-          var optionElement = document.createElement("option");
-          optionElement.text = optionText;
-          vocabularySelectorSelect.add(optionElement);
-        });
+function vocSelected() {
+  // console.log("new vocabulary selected");
+  vocSelected = document.getElementById("vocabularySelectorSelect").value;
+  // console.log(vocSelected);
 }
