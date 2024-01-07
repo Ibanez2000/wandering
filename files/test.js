1c3079d203395fd998aa42c6ec684f7f098f2796
Hauke Henning Selke <haukeselke@gmail.com>
2024-01-07 15:06:50 +0800

    test

---
+++ files/test.js
@@ -0,0 +1,95 @@
+{
+    JAanime: [
+      {
+        externalURL: "https://raw.githubusercontent.com/Ibanez2000/wandering/main/files/drillData/JAanime/",
+        fieldTypes: {
+          field0: "text",
+          field1: "text",
+          field2: "text",
+          field3: "text",
+          field4: "text",
+          field5: "text",
+          field6: "picture",
+          field7: "audio"
+        },
+        fieldVisibility: {
+          field0: "shown",
+          field1: "shown",
+          field2: "hidden",
+          field3: "hidden",
+          field4: "hidden",
+          field5: "hidden",
+          field6: "hidden",
+          field7: "hidden"
+        },
+        fieldNames: {
+          field0: "number",
+          field1: "hiragana",
+          field2: "kanji",
+          field3: "type",
+          field4: "meaning",
+          field5: "unused1",
+          field6: "unused2",
+          field7: "unused3"
+        }
+      },
+      {
+        content: [
+          {
+            number: 1,
+            hiragana: "むきわらばうし",
+            kanji: "麦わら帽子",
+            type: "(名)",
+            meaning: "straw hat",
+            unused1: "",
+            unused2: "",
+            unused3: ""
+          },
+          {
+            number: 2,
+            hiragana: "えをかく",
+            kanji: "絵を描く",
+            type: "(名)",
+            meaning: "to paint (draw) a picture",
+            romaji: "eokaku",
+            unused1: "",
+            unused2: "",
+            unused3: ""
+          },
+          {
+            number: 3,
+            hiragana: "ちきゅう",
+            kanji: "地球",
+            type: "(名)",
+            meaning: "Earth; the globe",
+            romaji: "chikyuu",
+            unused1: "",
+            unused2: "",
+            unused3: ""
+          },
+          {
+            number: 4,
+            hiragana: "もも",
+            kanji: "桃",
+            type: "(名)",
+            meaning: "peach",
+            romaji: "momo",
+            unused1: "",
+            unused2: "",
+            unused3: ""
+          },
+          {
+            number: 5,
+            hiragana: "わに",
+            kanji: "鰐",
+            type: "(名)",
+            meaning: "crocodile",
+            romaji: "wani",
+            unused1: "",
+            unused2: "",
+            unused3: ""
+          }
+        ]
+      }
+    ]
+  }
